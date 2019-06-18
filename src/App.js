c9d3b3317f959744992231400b15cbd651621e15
RMS <rene.as.himself@gmail.com>
2019-06-18 12:07:23 -0500

    Progress bar fix

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import styled from 'styled-components'
+import nprogress from 'nprogress'
 import Footer from './components/Footer'
 import Header from './components/Header'
 import Login from './components/Login'
 import Main from './components/Main'
 import Register from './components/Register'
-import nprogress from 'nprogress'
+
 import 'normalize.css'
+import './static/nprogress.css'
 import './index.scss'
-import '../static/nprogress.css'
 
 const AppStyles = styled.div`
     text-align: center;
@@ -22,30 +23,30 @@ const AppStyles = styled.div`
 
     @media all and (max-width: 500px) {
         grid-template-columns: auto minmax(auto, 1fr) auto;
-        grid-template-areas: 'header header header' '. main .' 'footer footer footer';
+        grid-template-areas:
+            'header header header'
+            '. main .'
+            'footer footer footer';
     }
 `
 
-class App extends Component {
+const App = () => {
     nprogress.start()
+    nprogress.done()
 
-    render() {
-        nprogress.done()
+    return (
+        <AppStyles className='App'>
+            <Header />
 
-        return (
-            <AppStyles className='App'>
-                <Header />
+            <Switch>
+                <Route path='/' component={Main} />
+                <Route path='/login' exact component={Login} />
+                <Route path='/register' exact component={Register} />
+            </Switch>
 
-                <Switch>
-                    <Route path='/' exact component={Main} />
-                    <Route path='/login' exact component={Login} />
-                    <Route path='/register' exact component={Register} />
-                </Switch>
-
-                <Footer />
-            </AppStyles>
-        )
-    }
+            <Footer />
+        </AppStyles>
+    )
 }
 
 export default App
