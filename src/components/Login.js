b56c08af8de0c70680775d83603b00830bd8ef9b
RMS <rene.as.himself@gmail.com>
2019-06-25 18:34:21 -0500

    Formatting

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,55 +5,55 @@ const LoginStyles = styled.section`
     grid-area: main;
 `
 
-export default function Login() {
-    return (
-        <LoginStyles className='section'>
-            <div className='columns'>
-                <div className='column is-4 is-offset-4'>
-                    <div className='field'>
-                        <label className='label' htmlFor='email'>
-                            <p className='control has-icons-left has-icons-right'>
-                                <input
-                                    required
-                                    name='email'
-                                    className='input'
-                                    type='email'
-                                    placeholder='Email'
-                                />
-                                <span className='icon is-small is-left'>
-                                    <i className='fa fa-envelope'></i>
-                                </span>
-                                <span className='icon is-small is-right'>
-                                    <i className='fa fa-check'></i>
-                                </span>
-                            </p>
-                        </label>
-                    </div>
-                    <div className='field'>
-                        <label className='label' htmlFor='password'>
-                            <p className='control has-icons-left'>
-                                <input
-                                    required
-                                    name='password'
-                                    className='input'
-                                    type='password'
-                                    placeholder='Password'
-                                />
-                                <span className='icon is-small is-left'>
-                                    <i className='fa fa-lock'></i>
-                                </span>
-                            </p>
-                        </label>
-                    </div>
-                    <div className='field'>
-                        <p className='control'>
-                            <button type='button' className='button is-success'>
-                                Login
-                            </button>
+const Login = () => (
+    <LoginStyles className='section'>
+        <div className='columns'>
+            <div className='column is-4 is-offset-4'>
+                <div className='field'>
+                    <label className='label' htmlFor='email'>
+                        <p className='control has-icons-left has-icons-right'>
+                            <input
+                                required
+                                name='email'
+                                className='input'
+                                type='email'
+                                placeholder='Email'
+                            />
+                            <span className='icon is-small is-left'>
+                                <i className='fa fa-envelope'></i>
+                            </span>
+                            <span className='icon is-small is-right'>
+                                <i className='fa fa-check'></i>
+                            </span>
                         </p>
-                    </div>
+                    </label>
+                </div>
+                <div className='field'>
+                    <label className='label' htmlFor='password'>
+                        <p className='control has-icons-left'>
+                            <input
+                                required
+                                name='password'
+                                className='input'
+                                type='password'
+                                placeholder='Password'
+                            />
+                            <span className='icon is-small is-left'>
+                                <i className='fa fa-lock'></i>
+                            </span>
+                        </p>
+                    </label>
+                </div>
+                <div className='field'>
+                    <p className='control'>
+                        <button type='button' className='button is-success'>
+                            Login
+                        </button>
+                    </p>
                 </div>
             </div>
-        </LoginStyles>
-    )
-}
+        </div>
+    </LoginStyles>
+)
+
+export default Login
