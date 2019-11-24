83f20cf8d97c1f7c7eb61ce473a566f0eb8bdc94
RMS <rene.as.himself@gmail.com>
2019-11-23 20:27:31 -0600

    Update Footer.tsx
    Renamed theme context variables

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,32 +5,30 @@ import Toggle from './Toggle/Toggle'
 
 const FooterStyles = styled.div`
     grid-area: footer;
+
+    footer {
+        padding: 1rem;
+    }
 `
 
 interface Props {}
 
 const Footer: React.FC<Props> = () => {
-    const { darkMode, setDarkMode } = useContext(ThemeContext)
+    const { theme, toggleTheme } = useContext(ThemeContext)
 
     return (
         <FooterStyles>
-            <footer className='footer'>
+            <footer className={'footer'}>
                 <div className='content has-text-centered'>
-                    <p>
-                        <strong>Bulma</strong> by <a href='https://jgthms.com'>Jeremy Thomas</a>.
-                        The source code is licensed
-                        <span>
-                            {' '}
-                            <a href='http://opensource.org/licenses/mit-license.php'>MIT</a>
-                        </span>
-                        . The website content is licensed{' '}
-                        <a href='http://creativecommons.org/licenses/by-nc-sa/4.0/'>
-                            CC BY NC SA 4.0
+                    <span className='credits'>
+                        Built with <span className='emoticon has-text-danger'>❤</span>
+                        {' by '}
+                        <a href='https://renems.com' target='_blank' rel='noopener noreferrer'>
+                            RMS
                         </a>
-                        .
-                    </p>
+                    </span>
 
-                    <Toggle isOn={darkMode} handleToggle={setDarkMode} />
+                    <Toggle isOn={theme === 'dark' ? true : false} handleToggle={toggleTheme} />
                 </div>
             </footer>
         </FooterStyles>
