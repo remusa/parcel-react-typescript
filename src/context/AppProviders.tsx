a9d3ee9a4eaa225bbe00921721306fb506f69c55
R Sánchez <rene.as.himself@gmail.com>
2019-11-27 12:32:07 -0600

    Added translations with react-intl

--- src/context/AppProviders.tsx
+++ src/context/AppProviders.tsx
@@ -1,10 +1,27 @@
 import React from 'react'
-import { ThemeProvider } from './ThemeContext'
+import { translationMessages } from './Language/i18n'
+import { LanguageProvider } from './Language/LanguageContext'
+import { ThemeProvider } from './Theme/ThemeContext'
+// import { BrowserRouter as Router } from 'react-router-dom'
+// import { HelmetProvider, Helmet } from 'react-helmet-async'
 
 interface Props {
-    children: HTMLElement
+  children: HTMLElement | any // ReactNode
 }
 
-const AppProviders: React.FC<Props> = ({ children }) => <ThemeProvider>{children}</ThemeProvider>
+const AppProviders: React.FC<Props> = ({ children }) => (
+  <LanguageProvider messages={translationMessages}>
+    <ThemeProvider>{children}</ThemeProvider>
+  </LanguageProvider>
+)
 
 export default AppProviders
+
+/*
+<HelmetProvider>
+    <Helmet>
+        <meta name="description" content="" />
+    </Helmet>
+    <App />
+</HelmetProvider>
+*/
